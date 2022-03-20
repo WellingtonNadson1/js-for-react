5bb5ffb250cd2d639c03c33ba96d212ecd48a28e
WellingtonNadson1 <wellingtonnadson@gmail.com>
2022-03-19 23:29:57 -0300

    :hammer: Now is posssible change the Card Image.

--- src/components/cardGame/index.js
+++ src/components/cardGame/index.js
@@ -1,9 +1,9 @@
 import './style.css'
 
-function CardGame(){
+function CardGame(logo ="logo-shield.png", alt = "Shield Logo"){
     return /*html*/`
         <article class="card-game">
-            <img class="logo-card-game" src="./src/img/logo-shield.png" alt="Shield Logo">
+            <img class="logo-card-game" src="./src/img/${logo}" alt=${alt}>
         </article>
     `
 };
