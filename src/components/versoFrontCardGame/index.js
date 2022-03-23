67db5e0f8ea92c5ae7c87afcbb184cd58ecc8030
WellingtonNadson1 <wellingtonnadson@gmail.com>
2022-03-23 16:42:59 -0300

    :construction: Improvements at layout with add components

--- src/components/versoFrontCardGame/index.js
+++ src/components/versoFrontCardGame/index.js
@@ -3,14 +3,19 @@ import './style.css';
 
 
 function versoFrontCardGame(){
+    // I used a DOM Object (window) for manipulatio the event onClick
+    // Named especific the object for use only here
     window.versoFrontCardGame = {};
     window.versoFrontCardGame.handleClick = (event) =>{
+        // I get the element clicked
         const $origin = event.target;
         console.log($origin)
+
         const $versoFrontCardGame = $origin.closest('.verso-front-card-game');
 
         $versoFrontCardGame.classList.toggle('-active');
-    }
+
+    };    
 
     return /*html*/`
         <article class="verso-front-card-game" onClick = "versoFrontCardGame.handleClick(event)">
--- src/objects/scoreBoard/index.js
+++ src/objects/scoreBoard/index.js
@@ -1,17 +1,22 @@
 import './style.css'
 import Player from '../../components/player';
 import VersusPlayerScore from '../../components/versusPlayerScore';
+import PlayerScore from '../../components/playerScore';
+import ArrowSocre from '../../components/arrowScore';
 
 function ScoreBoard(){
 
     return /*html*/`
+        ${ArrowSocre()}
         <section class="players">
             <div class="player1">
                 <h2>
                     ${Player("Player1")}
                 </h2>
             </div>
+            ${PlayerScore()}
             ${VersusPlayerScore()}
+            ${PlayerScore()}
             <div class="player2">
                 <h2>
                     ${Player("Player2")}
