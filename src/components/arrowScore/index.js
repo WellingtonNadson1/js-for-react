f5fbc929ce9b0c1665b4709303b0b9df1a4fffdd
WellingtonNadson1 <wellingtonnadson@gmail.com>
2022-03-23 16:41:23 -0300

    :sparkles: I created arrow indicator score of players

---
+++ src/components/arrowScore/index.js
@@ -0,0 +1,14 @@
+import './style.css'
+
+function ArrowSocre(){
+
+    return /*html*/`
+        <section class="box-score player">
+            <article class="arrow-score">
+                <img src="./src/img/IconArrowDown.svg" alt="Arrow Placar Score">
+            </article>
+        </section>
+    `
+};
+
+export default ArrowSocre
