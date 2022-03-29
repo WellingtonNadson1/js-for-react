f67002d81e0a0f1c7807f6bf0dd15163a6b0d3e4
WellingtonNadson1 <wellingtonnadson@gmail.com>
2022-03-28 23:41:35 -0300

    :building_construction: Add alt correct for images

--- src/components/cardGame/index.js
+++ src/components/cardGame/index.js
@@ -4,7 +4,7 @@ import './style.css'
 function CardGame(logo ="./src/img/logo-shield.png", alt = "Shield Logo", cl = ""){
     return /*html*/`
         <article class="card-game${cl}">
-            <img class="logo-card-game" src="${logo}" alt=${alt}>
+            <img class="logo-card-game" src="${logo}" alt="${alt}">
         </article>
     `
 };
