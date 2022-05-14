37a8d0173c3a62b960cd3321568b9b95fc2d15f7
WellingtonNadson1 <wellingtonnadson@gmail.com>
2022-05-14 12:47:08 -0300

    :lipstick: Change path directory img for images

--- src/components/arrowScore/index.js
+++ src/components/arrowScore/index.js
@@ -5,7 +5,7 @@ function ArrowSocre(){
     return /*html*/`
         <section data-arrowPlayerNow="2" class="box-score player">
             <article class="arrow-score">
-                <img src="../../img/IconArrowDown.svg" alt="Arrow Placar Score">
+                <img src="src/images/IconArrowDown.svg" alt="Arrow Placar Score">
             </article>
         </section>
     `
