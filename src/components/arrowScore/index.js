0da844661fdbcee7a114d57475332c0357029c06
WellingtonNadson1 <wellingtonnadson@gmail.com>
2022-05-14 11:45:10 -0300

    :recycle: Correction bug in the image path
    #

--- src/components/arrowScore/index.js
+++ src/components/arrowScore/index.js
@@ -5,7 +5,7 @@ function ArrowSocre(){
     return /*html*/`
         <section data-arrowPlayerNow="2" class="box-score player">
             <article class="arrow-score">
-                <img src="./src/img/IconArrowDown.svg" alt="Arrow Placar Score">
+                <img src="src/img/IconArrowDown.svg" alt="Arrow Placar Score">
             </article>
         </section>
     `
